440335d12bf7d3bab5ad35860e61d8adffbc9ff9
Yevhen Vlasenko <igusarr@gmail.com>
2025-07-28 23:46:47 +0300

    config

--- my-planer-front-end/next.config.ts
+++ my-planer-front-end/next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
 	basePath: '/1planer',
+	trailingSlash: true,
+	reactStrictMode: true,
+	// Если используешь images
+	images: {
+		unoptimized: true, // Отключаем оптимизацию, если нет домена для images
+	},
+	// Если используешь rewrites или redirects
+	async redirects() {
+		return [];
+	},
+	async rewrites() {
+		return [];
+	},
 };
 
 export default nextConfig;

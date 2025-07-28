e42b6652fde4f1e4e4e7e89f118cd0d769f3292c
Yevhen Vlasenko <igusarr@gmail.com>
2025-07-29 02:35:23 +0300

    cors settings

--- my-planer-back-end/src/main.ts
+++ my-planer-back-end/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['http://localhost:3000', 'https://1planer.stage-dream.tech'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})

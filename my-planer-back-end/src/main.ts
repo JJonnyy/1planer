7aaaad472eb0da654b2a936ff5895add03114db3
Yevhen Vlasenko <igusarr@gmail.com>
2025-08-03 16:31:22 +0300

    fix2

--- my-planer-back-end/src/main.ts
+++ my-planer-back-end/src/main.ts
@@ -1,18 +1,22 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import * as cookieParser from 'cookie-parser'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	app.setGlobalPrefix('api')
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api');
+
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-	app.use(cookieParser())
+	app.use(cookieParser());
+
 	app.enableCors({
+		// Исправлено: убраны лишние пробелы
 		origin: ['http://localhost:3000', 'https://1planer.stage-dream.tech'],
 		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+		exposedHeaders: 'set-cookie',
+	});
 
-	await app.listen(process.env.PORT ?? 4200, '0.0.0.0')
+	// Убедись, что сервер слушает на всех интерфейсах
+	await app.listen(process.env.PORT ?? 4200, '0.0.0.0');
 }
-bootstrap()
+bootstrap();
